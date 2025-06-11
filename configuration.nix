4d47eb0e05e46bde5e664bf85fba0f91edb2b1f0
Tom Herbers <mail@tomherbers.de>
2025-06-11 15:16:52 +0200

    configuration: gnome pkgs got move to top level

--- configuration.nix
+++ configuration.nix
@@ -46,7 +46,6 @@
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
     gnome-tour
-  ]) ++ (with pkgs.gnome; [
     cheese # webcam tool
     epiphany # Browser
     gnome-music
